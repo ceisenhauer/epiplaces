e359f1e26483557900dd7e365238daec2f247cbe
ceisenhauer <eisenhauer.catherine@gmail.com>
2022-07-14 21:57:59 +0100

    drc done!

---
+++ R/data_descriptions.R
@@ -0,0 +1,47 @@
+#' DRC : Zone Level
+#'
+#' Health zone level sf data for the Democratic Republic of the Congo.
+#'
+#' @docType data
+#'
+#' @format A `Simple feature collection` with 519 features and 4 fields: 
+#' \describe{
+#'  \item{country}{Country name}
+#'  \item{reg}{Region (province) name}
+#'  \item{area}{Region area (m2)}
+#'  \item{reg_display}{Title case version of `reg`}
+#' }
+#'
+#' @keywords dataset
+#'
+#' @source ...unclear.
+#'
+#' @examples
+#' sp::plot(sf_drc_reg)
+'sf_drc_reg'
+
+
+#' DRC : Zone Level
+#'
+#' Health zone level sf data for the Democratic Republic of the Congo.
+#'
+#' @docType data
+#'
+#' @format A `Simple feature collection` with 519 features and 6 fields: 
+#' \describe{
+#'  \item{country}{Country name}
+#'  \item{reg}{Region (province) name}
+#'  \item{zone}{Zone name (health zone)}
+#'  \item{area}{Zone area (m2)}
+#'  \item{reg_display}{Title case version of `reg`}
+#'  \item{zone_display}{Title case version of `zone`}
+#' }
+#'
+#' @keywords dataset
+#'
+#' @source ...unclear.
+#'
+#' @examples
+#' sp::plot(sf_drc_zone)
+'sf_drc_zone'
+

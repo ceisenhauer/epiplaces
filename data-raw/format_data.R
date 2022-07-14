7b710a4a948f050da31b346e2a18c44605b6412d
ceisenhauer <eisenhauer.catherine@gmail.com>
2022-07-14 21:17:07 +0100

    still empty...

---
+++ data-raw/format_data.R
@@ -0,0 +1,13 @@
+#' -------------------------------------------------------------------------------------------------
+#' Format Raw Data
+#' 
+#' @author Catherine Eisenhauer
+#' @date July 2022
+#'
+#' @description This is a data package, so pretty much all code is centralized in this folder. To
+#' keep things readable, each country has it's own file(s) where name cleaning is done. This script
+#' then runs all of those country specific files and saves their output.
+#'
+#' @suggests 
+#'   dplyr, sf, tinker
+#' -------------------------------------------------------------------------------------------------

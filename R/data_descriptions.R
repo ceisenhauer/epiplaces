bc2d0a197c028fc5026b160ed8102c84dfe78b2a
ceisenhauer <eisenhauer.catherine@gmail.com>
2022-07-18 21:42:16 +0100

    update

--- R/data_descriptions.R
+++ R/data_descriptions.R
@@ -1,3 +1,30 @@
+#' Afghanistan
+#'
+#' Health district level sf data for Afghanistan.
+#'
+#' @docType data
+#'
+#' @format A `Simple feature collection` with 398 features and 4 fields: 
+#' \describe{
+#'  \item{country}{Country name}
+#'  \item{zone}{Zone name (health district)}
+#'  \item{area}{Zone area (m2)}
+#'  \item{zone_display}{Title case version of `zone`}
+#' }
+#'
+#' @keywords dataset
+#'
+#' @source [geoBoundaries](https://www.geoboundaries.org) via 
+#'   [humdata](https://data.humdata.org/) (2020).
+
+#'
+#' @examples
+#' ggplot2::ggplot(sf::st_as_sf(afghanistan_zone)) +
+#'   ggplot2::geom_sf() +
+#'   ggplot2::theme_void()
+'afghanistan_zone'
+
+
 #' Democratic Republic of the Congo
 #'
 #' National level sf data for the Democratic Republic of the Congo.
@@ -13,10 +40,12 @@
 #'
 #' @keywords dataset
 #'
-#' @source ...unclear.
+#' @source Legacy (ie. I don't know).
 #'
 #' @examples
-#' plot(sf::st_as_sf(drc_nat))
+#' ggplot2::ggplot(sf::st_as_sf(drc_nat)) +
+#'   ggplot2::geom_sf() +
+#'   ggplot2::theme_void()
 'drc_nat'
 
 #' Democratic Republic of the Congo
@@ -35,10 +64,12 @@
 #'
 #' @keywords dataset
 #'
-#' @source ...unclear.
+#' @source Legacy (ie. I don't know).
 #'
 #' @examples
-#' plot(sf::st_as_sf(drc_reg))
+#' ggplot2::ggplot(sf::st_as_sf(drc_reg)) +
+#'   ggplot2::geom_sf() +
+#'   ggplot2::theme_void()
 'drc_reg'
 
 
@@ -60,9 +91,86 @@
 #'
 #' @keywords dataset
 #'
-#' @source ...unclear.
+#' @source Legacy (ie. I don't know).
 #'
 #' @examples
-#' plot(sf::st_as_sf(drc_zone))
+#' ggplot2::ggplot(sf::st_as_sf(drc_zone)) +
+#'   ggplot2::geom_sf() +
+#'   ggplot2::theme_void()
 'drc_zone'
+
+
+#' Liberia
+#'
+#' National level sf data for Liberia.
+#'
+#' @docType data
+#'
+#' @format A `Simple feature collection` with 1 feature and 3 fields: 
+#' \describe{
+#'  \item{country}{Country name}
+#'  \item{area}{Zone area (m2)}
+#'  \item{country_display}{Full and formatted name of `country`}
+#' }
+#'
+#' @keywords dataset
+#'
+#' @source OCHA West and Central AFrica via [humdata](https://data.humdata.org/) (2022).
+#'
+#' @examples
+#' ggplot2::ggplot(sf::st_as_sf(liberia_zone)) +
+#'   ggplot2::geom_sf() +
+#'   ggplot2::theme_void()
+'liberia_nat'
+
+#' Liberia
+#'
+#' Health zone level sf data for the Democratic Republic of the Congo.
+#'
+#' @docType data
+#'
+#' @format A `Simple feature collection` with 15 features and 4 fields: 
+#' \describe{
+#'  \item{country}{Country name}
+#'  \item{reg}{Region (county) name}
+#'  \item{area}{Region area (m2)}
+#'  \item{reg_display}{Title case version of `reg`}
+#' }
+#'
+#' @keywords dataset
+#'
+#' @source OCHA West and Central AFrica via [humdata](https://data.humdata.org/) (2022).
+#'
+#' @examples
+#' ggplot2::ggplot(sf::st_as_sf(liberia_zone)) +
+#'   ggplot2::geom_sf() +
+#'   ggplot2::theme_void()
+'liberia_reg'
+
+
+#' Liberia
+#'
+#' Health zone level sf data for the Democratic Republic of the Congo.
+#'
+#' @docType data
+#'
+#' @format A `Simple feature collection` with 136 features and 6 fields: 
+#' \describe{
+#'  \item{country}{Country name}
+#'  \item{reg}{Region (county) name}
+#'  \item{zone}{Zone name (health district)}
+#'  \item{area}{Zone area (m2)}
+#'  \item{reg_display}{Title case version of `reg`}
+#'  \item{zone_display}{Title case version of `zone`}
+#' }
+#'
+#' @keywords dataset
+#'
+#' @source OCHA West and Central AFrica via [humdata](https://data.humdata.org/) (2022).
+#'
+#' @examples
+#' ggplot2::ggplot(sf::st_as_sf(liberia_zone)) +
+#'   ggplot2::geom_sf() +
+#'   ggplot2::theme_void()
+'liberia_zone'
 
